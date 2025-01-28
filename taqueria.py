5e1a5d1b8962efaccc44f56e496e592e6beaf669
Anuj Sanvatsarkar <sanvatsarkaranuj13@gmail.com>
2025-01-27 19:42:29 -0500

    Update taqueria.py

--- taqueria.py
+++ taqueria.py
@@ -1,1 +1,25 @@
+# python taqueria.py
+dir = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
 
+total = 0
+while True:
+    try:
+        order = input("Item: ").strip().title()
+        if order in dir:
+            total += dir[order]
+            print(f"Total: ${total:.2f}")
+        else:
+            print("", end = "\r")
+    except EOFError:
+        print()
+        break
